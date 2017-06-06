e777825680ff86ab4ace99973d4ec4994387a79e
James Byars <jamesbyars@g.harvard.edu>
2017-06-06 16:15:33 -0500

    Initial Commit

---
+++ src/main/java/com/ernesttech/example/databaseascode/DatabaseAsCodeApplication.java
@@ -0,0 +1,12 @@
+package com.ernesttech.example.databaseascode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseAsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseAsCodeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ernesttech/example/databaseascode/DatabaseAsCodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ernesttech.example.databaseascode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatabaseAsCodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
